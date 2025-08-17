a31f476c7f079fbe504692c1e721b9abd74db48a
J-sense <jishan1873@gmail.com>
2025-08-17 12:18:49 +0600

    feat(subscriber, dashboard): add state, API fetching, and user detail view button

--- src/pages/super-admin/screen_admin_management.tsx
+++ src/pages/super-admin/screen_admin_management.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { IconCustomerInfo } from "../../components/icons";
 import { ButtonStatus, TextSearchBox } from "../../components/input";
 import { ModalAddSubscriber } from "../../components/modals";
@@ -8,16 +9,30 @@ import {
   hideSubscriberDetail,
   showSubscriberDetail,
 } from "./redux-slices/slice_admin_management";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/store";
+import axiosInstance from "@/lib/axios";
 
-interface Subscriber {
+export interface TSubscriber {
+  id: number;
+  resturent_name: string;
+  location: string;
+  phone_number: string;
+  image: string;
+  owner: number;
+  package: string | null;
+  subscriptions: any[];
+  created_at: string;
+  updated_at: string;
+  status?: "Active" | "Hold" | "Inactive" | string;
+}
+export interface Subscriber {
   name: string;
   customerId: string;
   cellNumber: string;
   startingDate: string;
-  package: string;
-  status: "Active" | "Hold" | "Pending";
+  package: "Monthly" | "Half yearly" | "Yearly" | string;
+  status: "Active" | "Hold" | "Inactive" | string;
 }
 export const subscribers: Subscriber[] = [
   {
@@ -53,7 +68,43 @@ export const subscribers: Subscriber[] = [
     status: "Active",
   },
 ];
+
 const ScreenAdminManagement = () => {
+  type DashboardStats = {
+    total_restaurant: number;
+    total_hold_restaurant: number;
+    total_active_restaurant: number;
+  };
+  const [allResutrentUser, setallResutrentUser] = useState<TSubscriber[]>([]);
+
+  const [stats, setStats] = useState<DashboardStats>({
+    total_restaurant: 0,
+    total_hold_restaurant: 0,
+    total_active_restaurant: 0,
+  });
+  useEffect(() => {
+    const fetchDatas = async () => {
+      const res = await axiosInstance.get("/adminapi/restaurants/summary/");
+      const data = await res?.data;
+      setStats(data);
+    };
+    fetchDatas();
+  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosInstance.get("/adminapi/restaurants/");
+        console.log(response.data.results);
+        const result = response.data.results;
+        setallResutrentUser(result);
+        console.log(subscribers);
+      } catch (error) {
+        console.error("Error fetching subscribers", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   const dispatch = useAppDispatch();
 
   const showAddSubscriberModal = () => {
@@ -67,7 +118,7 @@ const ScreenAdminManagement = () => {
           {/* Card 1 */}
           <StatCardAsteric
             label="Total Restaurant"
-            data={"100"}
+            data={stats.total_restaurant.toString()}
             accentColor="#FFB056"
             gradientStart="#48E03A"
             gradientEnd="#161F42"
@@ -75,7 +126,7 @@ const ScreenAdminManagement = () => {
           {/* Card 2 */}
           <StatCardAsteric
             label="Total On Hold"
-            data={"20"}
+            data={stats.total_hold_restaurant.toString()}
             accentColor="#FF6561"
             gradientStart="#FFB056"
             gradientEnd="#161F42"
@@ -84,7 +135,7 @@ const ScreenAdminManagement = () => {
           <StatCardAsteric
             accentColor="#31BB24"
             label="Active Today"
-            data={"12"}
+            data={stats.total_active_restaurant.toString()}
             gradientStart="#EB342E"
             gradientEnd="#161F42"
           />
@@ -94,19 +145,19 @@ const ScreenAdminManagement = () => {
         <div className="flex flex-row justify-between items-center my-3">
           <h2 className="text-2xl text-primary-text">Subscriber management</h2>
           <div className="flex flex-row justify-end items-center gap-x-4">
-            <button
+            {/* <button
               onClick={() => showAddSubscriberModal()}
               className="button-primary bg-sidebar text-nowrap flex flex-row justify-center items-center h-14 gap-x-2"
             >
               <IoMdAdd />
               <span>Add Subscriber</span>
-            </button>
+            </button> */}
             <TextSearchBox placeholder="Search subscriber" />
           </div>
         </div>
         {/* List of content */}
         <div className="bg-sidebar p-4 rounded-lg">
-          <TableSubscriberList subscribers={subscribers} />
+          <TableSubscriberList subscribers={allResutrentUser} />
           <div className="mt-4 flex justify-center">
             <Pagination page={1} />
           </div>
@@ -116,11 +167,23 @@ const ScreenAdminManagement = () => {
   );
 };
 interface TableSubscriberListProps {
-  subscribers: Subscriber[];
+  subscribers: TSubscriber[];
 }
 export const TableSubscriberList: React.FC<TableSubscriberListProps> = ({
   subscribers,
 }) => {
+  type DashboardStats = {
+    total_restaurant: number;
+    total_hold_restaurant: number;
+    total_active_restaurant: number;
+  };
+  const [resUserId, setResUserId] = useState<string | undefined>(undefined);
+  const [stats, setStats] = useState<DashboardStats>({
+    total_restaurant: 0,
+    total_hold_restaurant: 0,
+    total_active_restaurant: 0,
+  });
+  console.log(stats, "states");
   const dispatch = useAppDispatch();
   const openShowDetailModal = useAppSelector(
     (state) => state.adminManagement.modals.showDetail
@@ -128,8 +191,18 @@ export const TableSubscriberList: React.FC<TableSubscriberListProps> = ({
   const openAddSubscriberModal = useAppSelector(
     (state) => state.adminManagement.modals.addSubscriber
   );
+  useEffect(() => {
+    const fetchDatas = async () => {
+      const res = await axiosInstance.get("/adminapi/restaurants/summary/");
+      const data = await res?.data;
+      console.log(data);
+      setStats(data);
+    };
+    fetchDatas();
+  }, []);
 
-  const showAddSubscriberModal = () => {
+  const showAddSubscriberModal = (id: string) => {
+    setResUserId(id);
     dispatch(showSubscriberDetail(undefined));
   };
 
@@ -154,14 +227,16 @@ export const TableSubscriberList: React.FC<TableSubscriberListProps> = ({
         <tbody className="bg-sidebar text-sm">
           {subscribers.map((item, index) => (
             <tr key={index} className="border-b border-[#1C1E3C]">
-              <td className="p-4 text-primary-text">{item.name}</td>
-              <td className="p-4 text-primary-text">{item.customerId}</td>
-              <td className="p-4 text-primary-text">{item.cellNumber}</td>
-              <td className="p-4 text-primary-text">{item.startingDate}</td>
-              <td className="p-4 text-primary-text">{item.package}</td>
+              <td className="p-4 text-primary-text">{item?.resturent_name}</td>
+              <td className="p-4 text-primary-text">{item?.id}</td>
+              <td className="p-4 text-primary-text">{item?.phone_number}</td>
+              <td className="p-4 text-primary-text">{item?.created_at}</td>
+              <td className="p-4 text-primary-text">
+                {item?.package || "loading"}
+              </td>
               <td className="h-20 p-4 flex gap-x-4 items-center">
                 <button
-                  onClick={() => showAddSubscriberModal()}
+                  onClick={() => showAddSubscriberModal(item?.id.toString())}
                   className="text-blue-100 bg-table-header flex flex-row items-center gap-x-2 p-3"
                 >
                   <IconCustomerInfo />
@@ -176,7 +251,7 @@ export const TableSubscriberList: React.FC<TableSubscriberListProps> = ({
                     "Pending",
                     "Delete Request",
                   ]}
-                  status={item.status}
+                  status={item?.status || "Active"}
                   properties={{
                     Active: {
                       bg: "bg-green-800",
@@ -202,6 +277,7 @@ export const TableSubscriberList: React.FC<TableSubscriberListProps> = ({
         </tbody>
       </table>
       <ModalAddSubscriber
+        id={resUserId ? Number(resUserId) : undefined}
         isOpen={openShowDetailModal}
         close={hideAddSubscriberModal}
       />
